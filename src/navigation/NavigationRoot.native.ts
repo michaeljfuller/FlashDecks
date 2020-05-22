be878630f9a067fea518718f7eb20a324328955f
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2020-05-22 15:24:55 +0100

    Mask typings warning in NavigationRoot.native.

--- src/navigation/NavigationRoot.native.ts
+++ src/navigation/NavigationRoot.native.ts
@@ -2,5 +2,5 @@ import { createAppContainer } from "react-navigation";
 import { AppContainer } from '../screens/AppContainer';
 
 /** Make AppContainer the root of Navigation. */
-export const NavigationRoot = createAppContainer(AppContainer);
+export const NavigationRoot = createAppContainer(AppContainer as any);
 export default NavigationRoot;
