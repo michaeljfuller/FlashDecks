4ed2a5ece90d2885f84b07f4342b4ac5f07859fa
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2020-06-05 16:46:58 +0100

    Handle more auth events.

--- src/AppRoot/AppRootBase.ts
+++ src/AppRoot/AppRootBase.ts
@@ -9,16 +9,24 @@ export interface AppRootState {
     cognitoUser?: CognitoUser;
 }
 
-interface AuthEvent {
+/** https://aws-amplify.github.io/amplify-js/api/globals.html#hubcapsule */
+interface HubCapsule {
     channel: 'auth';
     source: 'Auth';
     payload: {
         event: AuthEventType;
-        message: string;
-        data: any|CognitoUser;
+        message?: string;
+        data?: any|CognitoUser;
     };
 }
-enum AuthEventType { SIGN_IN = 'signIn', SIGN_UP = 'signUp', SIGN_OUT = 'signOut' }
+/** https://docs.amplify.aws/lib/utilities/hub/q/platform/js#authentication-events */
+enum AuthEventType {
+    SIGN_IN = 'signIn',
+    SIGN_UP = 'signUp',
+    SIGN_OUT = 'signOut',
+    SIGN_IN_FAILED = 'signIn_failure',
+    CONFIGURED = 'configured'
+}
 
 export abstract class AppRootBase extends React.Component<AppRootProps, AppRootState> {
     state: AppRootState = {
@@ -28,19 +36,22 @@ export abstract class AppRootBase extends React.Component<AppRootProps, AppRootS
 
     componentDidMount() {
         this.fetchUserData();
-        Hub.listen('auth', data => this.onAuthEvent(data as AuthEvent));
+        Hub.listen('auth', capsule => this.onAuthEvent(capsule as HubCapsule));
     }
 
     /**
      * @link https://docs.amplify.aws/lib/utilities/hub/q/platform/js
      */
-    onAuthEvent(data: AuthEvent) {
-        console.info('AppRoot.onAuthEvent', data);
-        switch(data.payload.event) {
+    onAuthEvent(capsule: HubCapsule) {
+        console.info('AppRoot.onAuthEvent', capsule);
+        const {event, message} = capsule.payload;
+        switch(event) {
             case AuthEventType.SIGN_IN: return this.fetchUserData();
             case AuthEventType.SIGN_UP: return;
             case AuthEventType.SIGN_OUT: return this.clearUser();
-            default: console.warn('AppRoot.onAuthEvent', 'No handled case for', data.payload.event);
+            case AuthEventType.SIGN_IN_FAILED: return console.warn('Failed to sign in.', message);
+            case AuthEventType.CONFIGURED: return console.warn('Auth module already configured', message);
+            default: console.warn('AppRoot.onAuthEvent', 'No handled case for', event);
         }
     }
 
