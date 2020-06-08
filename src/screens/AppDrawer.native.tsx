fa388b3728b082bc35eca445b90f9183a72d1419
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2020-06-08 11:20:56 +0100

    Move NativeBase Content
    Only supposed to be one `<Content>`, so moved from ScreenContainer (may be multiple in future if nested), to AppDrawer.

--- src/screens/AppDrawer.native.tsx
+++ src/screens/AppDrawer.native.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-import {Text, ScrollView} from "react-native";
+import {Text} from "react-native";
 import {AppNavigator} from './AppContainer_navigator';
 import {StandardNavigator, setNavigator} from "../navigation/navigators/StandardNavigator/StandardNavigator";
 import NavigatorContainer from "../navigation/NavigationContainer";
+import {Content} from 'native-base';
 
 @setNavigator(AppNavigator)
 export class AppDrawer extends StandardNavigator {
     render() {
         return <NavigatorContainer>
             <Text style={{backgroundColor: 'orange'}}>AppDrawer</Text>
-            {super.render()}
+            <Content>{super.render()}</Content>
         </NavigatorContainer>;
     }
 }
