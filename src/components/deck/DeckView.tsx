34e654c542582f7db0993ce4b43abff741e8c54f
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2020-06-21 13:06:59 +0100

    Start styling DeckView

--- src/components/deck/DeckView.tsx
+++ src/components/deck/DeckView.tsx
@@ -1,21 +1,61 @@
 import React from "react";
 import {Text, View} from "react-native";
+import Typography from '@material-ui/core/Typography';
 import DeckViewBase from "./DeckView.common";
 import CardView from "../card/CardView";
+import Avatar from "../avatar/Avatar";
+import Tag from "../tag/Tag";
 
 export interface DeckViewState {}
 export default class DeckView extends DeckViewBase<DeckViewState> {
     state: DeckViewState = {};
 
     render() {
+        const deck = this.props.item;
+
         return <View>
             <Text style={{ fontWeight: 'bold', textAlign: 'center' }}>DeckView</Text>
-            {Object.keys(this.props.item).map(key => <View key={key} style={{ flexDirection: 'row' }}>
-                <Text style={{ fontWeight: 'bold'}}>{key}: </Text>
-                <Text>{JSON.stringify((this.props.item as any)[key])}</Text>
-            </View>)}
+            {this.renderHeader()}
+            {this.renderTags()}
+            <Text>{deck.description}</Text>
             {this.renderCards()}
         </View>;
+    }
+
+    renderHeader() {
+        const deck = this.props.item;
+        const avatarSize = 35;
+
+        return <View style={{
+            height: avatarSize,
+            position: 'relative',
+        }}>
+            <Typography variant="h5" align="center">{deck.name}</Typography>
+            <View style={{
+                position: 'absolute',
+                flexDirection: 'row',
+            }}>
+                <Avatar user={deck.owner} style={{ size: avatarSize }} />
+                <Text style={{
+                    marginLeft: 5,
+                    lineHeight: avatarSize,
+                }}>{deck.owner.displayName}</Text>
+            </View>
+        </View>;
+    }
+
+    renderTags() {
+        const {tags} = this.props.item;
+
+        if (tags && tags.length) {
+            return <View style={{
+                flexDirection: 'row',
+                alignSelf: 'flex-end',
+            }}>
+                {tags.map(tag => <Tag key={tag} value={tag} />)}
+            </View>
+        }
+        return null;
     }
 
     renderCards() {
