aa15f993a39d187c98f15de668806e14c3b08101
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2020-07-04 16:59:52 +0100

    Update Expo SDK from 37 to 38
    https://dev.to/expo/expo-sdk-38-is-now-available-5aa0
    https://github.com/expo/expo/blob/master/CHANGELOG.md

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,15 @@
-const blacklist = require('metro').createBlacklist;
+const blacklist = require('metro-config/src/defaults/blacklist');
+
+// Print default blacklistRE
+// require('metro-config').getDefaultConfig().then(data => console.log('blacklistRE', data.resolver.blacklistRE));
+// /(node_modules\\react\\dist\\.*|website\\node_modules\\.*|heapCapture\\bundle\.js|.*\\__tests__\\.*)$/,
 
 module.exports = {
     resolver: {
         blacklistRE: blacklist([
-            /#current-cloud-backend\/.*/ // Blacklist `amplify/#current-cloud-backend` because it duplicates `amplify/backend`.
+            // Blacklist `amplify/#current-cloud-backend` because it duplicates `amplify/backend`.
+            // Otherwise a "jest-haste-map: Haste module naming collision:" error is raised.
+            /#current-cloud-backend\/.*/
         ]),
     },
 };
