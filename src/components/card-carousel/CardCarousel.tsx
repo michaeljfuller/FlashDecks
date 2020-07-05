ef9a6044581eb0e8818ae4fe54c8b696011c15f5
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2020-07-05 22:28:38 +0100

    Add controls to CardCarousel

--- src/components/card-carousel/CardCarousel.tsx
+++ src/components/card-carousel/CardCarousel.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, StyleSheet, Text} from 'react-native';
 import CardView from "../card/CardView";
 import {CardCarouselProps} from "./CardCarousel.common";
+import Button from "../button/Button";
 export * from "./CardCarousel.common";
 
 export function CardCarousel(props: CardCarouselProps) {
     const {cards, style} = props;
+    const [index, setIndex] = useState(0);
 
     if (!cards?.length) {
         return <View style={[styles.root, style]}>
             <Text>No cards found.</Text>
         </View>;
     }
+    const card = cards[index];
 
     return <View style={[styles.root, style]}>
-        {cards.map(card => {
-            return <View key={card.id} style={{ margin: 2 }}>
-                <CardView item={card} />
-            </View>;
-        })}
+        <Button title="<" onClick={() => setIndex(index-1)} disabled={index <= 0}/>
+        <View style={styles.card}>
+            <CardView item={card} />
+        </View>
+        <Button title=">" onClick={() => setIndex(index+1)} disabled={index+1 >= cards.length} />
     </View>;
 }
 export default CardCarousel;
@@ -28,5 +31,9 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: "row",
         backgroundColor: 'red',
+    },
+    card: {
+        marginVertical: "auto",
+        flex: 1,
     },
 });
