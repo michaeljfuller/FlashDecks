3f13011168c5349f973ef38d82fe108e29431aa6
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2020-07-06 00:09:56 +0100

    Add animation to CardCarousel

--- src/components/card-carousel/CardCarousel.tsx
+++ src/components/card-carousel/CardCarousel.tsx
@@ -1,28 +1,81 @@
-import React, {useState} from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import React from 'react';
+import {View, StyleSheet, Text, Animated} from 'react-native';
+import {isPlatformWeb} from "../../platform";
 import CardView from "../card/CardView";
 import {CardCarouselProps} from "./CardCarousel.common";
 import Button from "../button/Button";
 export * from "./CardCarousel.common";
 
-export function CardCarousel(props: CardCarouselProps) {
-    const {cards, style} = props;
-    const [index, setIndex] = useState(0);
+const useNativeDriver = !isPlatformWeb;
 
-    if (!cards?.length) {
+interface CardCarouselState {
+    index: number;
+}
+export class CardCarousel extends React.Component<CardCarouselProps, CardCarouselState>{
+    state = {
+        index: 0,
+    } as CardCarouselState;
+
+    // https://reactnative.dev/docs/animations
+    cardOpacity = new Animated.Value(1);
+    cardPosition = new Animated.Value(0);
+
+    async cardOut(endPosition: number, duration = 250) {
+        return new Promise(resolve => {
+            Animated.parallel([
+                Animated.timing(this.cardOpacity, { toValue: 0, duration, useNativeDriver }),
+                Animated.timing(this.cardPosition, { toValue: endPosition, duration, useNativeDriver }),
+            ]).start(resolve);
+        });
+    }
+    async cardIn(startPosition: number, duration = 250) {
+        return new Promise(resolve => {
+            Animated.sequence([
+                Animated.timing(this.cardPosition, {toValue: startPosition, duration: 0, useNativeDriver}),
+                Animated.parallel([
+                    Animated.timing(this.cardOpacity, {toValue: 1, duration, useNativeDriver}),
+                    Animated.timing(this.cardPosition, {toValue: 0, duration, useNativeDriver}),
+                ]),
+            ]).start(resolve);
+        });
+    }
+
+    next = async () => {
+        await this.cardOut(-300);
+        this.setState({ index: this.state.index + 1 });
+        await this.cardIn(300);
+    };
+
+    previous = async () => {
+        await this.cardOut(300);
+        this.setState({ index: this.state.index - 1 });
+        await this.cardIn(-300);
+    };
+
+    render() {
+        const {cards, style} = this.props;
+        const {index} = this.state;
+
+        if (!cards?.length) {
+            return <View style={[styles.root, style]}>
+                <Text>No cards found.</Text>
+            </View>;
+        }
+        const card = cards[index];
+
         return <View style={[styles.root, style]}>
-            <Text>No cards found.</Text>
+            <Button title="<" onClick={this.previous} disabled={index <= 0}/>
+            <View style={styles.cardContainer}>
+                <Animated.View style={{
+                    opacity: this.cardOpacity,
+                    [isPlatformNative ? 'translateX' : 'left']: this.cardPosition,
+                }}>
+                    <CardView item={card}/>
+                </Animated.View>
+            </View>
+            <Button title=">" onClick={this.next} disabled={index + 1 >= cards.length}/>
         </View>;
     }
-    const card = cards[index];
-
-    return <View style={[styles.root, style]}>
-        <Button title="<" onClick={() => setIndex(index-1)} disabled={index <= 0}/>
-        <View style={styles.card}>
-            <CardView item={card} />
-        </View>
-        <Button title=">" onClick={() => setIndex(index+1)} disabled={index+1 >= cards.length} />
-    </View>;
 }
 export default CardCarousel;
 
@@ -32,8 +85,9 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         backgroundColor: 'red',
     },
-    card: {
+    cardContainer: {
         marginVertical: "auto",
         flex: 1,
+        overflow: "hidden",
     },
 });
