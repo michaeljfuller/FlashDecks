a4ee5ff7d846dcf732f30cdf1201fd884d9c68db
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2020-07-07 16:44:29 +0100

    Re-center CardCarousel

--- src/components/deck/DeckView.tsx
+++ src/components/deck/DeckView.tsx
@@ -90,6 +90,7 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: "row",
         alignItems: "center",
+        marginHorizontal: 'auto',
         marginBottom: 5,
         width: "100%",
         maxWidth: 1200,
