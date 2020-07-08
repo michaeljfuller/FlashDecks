edc1dcd9257bd9ae2a51648a01235c5c0e51c84b
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2020-07-08 16:32:19 +0100

    Add ScrollView to content

--- src/components/card/CardView.tsx
+++ src/components/card/CardView.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import {Text, View} from "react-native";
+import {ScrollView, Text, View, StyleSheet} from "react-native";
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
 
 import {CardViewProps} from "./CardView.common";
 import {withStyles} from "@material-ui/core/styles";
@@ -11,44 +10,55 @@ import {Color} from "../../styles/Color";
 export default function CardView(props: CardViewProps) {
     return <View style={props.style}>
         <StyledCard variant="elevation" raised={true} elevation={5}>
+            <View style={styles.inner}>
 
-            <StyledCardHeader
-                title={props.item?.name || "Unknown"}
-                titleTypographyProps={{ style: { userSelect: "none" } }}
-            />
+                <StyledCardHeader
+                    title={props.item?.name || "Unknown"}
+                    titleTypographyProps={{ style: { userSelect: "none" } }}
+                />
 
-            <StyledCardContent>
-                <Text style={{ fontSize: 10 }} selectable={false}>{JSON.stringify(props.item, null, 2)}</Text>
-            </StyledCardContent>
+                <ScrollView style={styles.body}>
+                    <View style={styles.content}>
+                        <Text style={{ fontSize: 10 }} selectable={false}>{JSON.stringify(props.item, null, 2)}</Text>
+                    </View>
+                </ScrollView>
 
+            </View>
         </StyledCard>
     </View>;
 }
 
+const edgeRadius = 15;
 const StyledCard = withStyles({
     root: {
         flex: 1,
-        padding: 5,
-        paddingTop: 0,
+        flexDirection: "column",
+        padding: 0,
+        paddingBottom: edgeRadius,
+        borderRadius: edgeRadius,
         marginBottom: 5,
-        borderRadius: 15,
-        backgroundColor: Color.White,
-        borderColor: Color.Grey,
+        backgroundColor: Color.OffWhite,
     }
 })(Card) as typeof Card;
 
 const StyledCardHeader = withStyles({
     root: {
-        padding: 7,
-        paddingBottom: 0,
-        textAlign: 'center'
+        padding: 0,
+        textAlign: 'center',
     }
 })(CardHeader) as typeof CardHeader;
 
-const StyledCardContent = withStyles({
-    root: {
-        padding: 5,
-        paddingTop: 0,
-        paddingBottom: 0,
-    }
-})(CardContent) as typeof CardContent;
+const styles = StyleSheet.create({
+    inner: {
+        flexDirection: "column",
+        height: '100%',
+    },
+    body: {
+        borderColor: Color.Grey,
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+    },
+    content: {
+        backgroundColor: Color.White,
+    },
+});
