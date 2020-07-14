aaf65e83a5b148f79752cc3fd1af8ff4a3173ada
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2020-07-14 18:20:21 +0100

    Implement native CardMediaVideo
    Install expo-av

--- src/components/card/CardContent/media/CardMediaVideo.native.tsx
+++ src/components/card/CardContent/media/CardMediaVideo.native.tsx
@@ -1,10 +1,58 @@
 import React from "react";
-import {Text} from "react-native";
+import {View, LayoutChangeEvent} from "react-native";
+import {Video, VideoNaturalSize, VideoReadyForDisplayEvent} from 'expo-av';
 import {CardContentProps} from "../CardContent";
 
-export function CardMediaVideo(props: CardContentProps) {
-    return <React.Fragment>
-        <Text selectable={false}>CardMediaVideo</Text>
-        <Text selectable={false}>{props.content.value}</Text>
-    </React.Fragment>;
+export interface CardMediaVideoState {
+    videoSize: VideoNaturalSize;
+    viewWidth: number;
+    displaySize: Dimensions;
 }
+interface Dimensions {
+    width: number;
+    height: number;
+}
+export class CardMediaVideo extends React.Component<CardContentProps, CardMediaVideoState> {
+    state = {
+        videoSize: { width: 0, height: 0, orientation: "landscape" },
+        viewWidth: 0,
+        displaySize: { width: 0, height: 0 }
+    } as CardMediaVideoState;
+
+    onViewLayout = (event: LayoutChangeEvent) => {
+        this.setState({ viewWidth: event.nativeEvent.layout.width });
+        this.updateDisplaySize();
+    }
+    onVideoReadyForDisplay = (event: VideoReadyForDisplayEvent) => {
+        this.setState({ videoSize: event.naturalSize });
+        this.updateDisplaySize();
+    }
+    updateDisplaySize() {
+        const {viewWidth, videoSize} = this.state;
+        let width = viewWidth;
+        let height = videoSize.height;
+        if (videoSize.width > viewWidth && videoSize.height > 0) {
+            const videoAspectRatio = videoSize.width / videoSize.height;
+            width = viewWidth;
+            height = viewWidth / videoAspectRatio;
+        }
+        this.setState({ displaySize: { width, height } });
+    }
+
+    render() {
+        const width = this.state.displaySize.width || 100;
+        const height = this.state.displaySize.height || 100;
+
+        return <View onLayout={this.onViewLayout}>
+            <Video
+                source={{ uri: this.props.content.value }}
+                onReadyForDisplay={this.onVideoReadyForDisplay} style={{ width, height }}
+                isLooping={true}
+                shouldPlay={true}
+                resizeMode="contain"
+                useNativeControls={true}
+            />
+        </View>;
+    }
+}
+export default CardMediaVideo;
