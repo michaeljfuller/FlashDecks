a1750563916b35306640351e650ade9f60f92305
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2020-07-17 16:07:24 +0100

    Remove placeholder label from CardMediaText

--- src/components/card/CardContent/media/CardMediaText.tsx
+++ src/components/card/CardContent/media/CardMediaText.tsx
@@ -3,8 +3,5 @@ import {Text} from "react-native";
 import {CardContentProps} from "../CardContent";
 
 export function CardMediaText(props: CardContentProps) {
-    return <React.Fragment>
-        <Text selectable={false}>CardMediaText</Text>
-        <Text selectable={false}>{props.content.value}</Text>
-    </React.Fragment>;
+    return <Text selectable={false}>{props.content.value}</Text>;
 }
