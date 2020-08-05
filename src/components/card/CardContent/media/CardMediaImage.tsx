df316588940f3d60c0c5689358fa70e67a16001a
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2020-08-05 20:36:28 +0100

    Allow CardMeriaImage/Video to change source.

--- src/components/card/CardContent/media/CardMediaImage.tsx
+++ src/components/card/CardContent/media/CardMediaImage.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useCallback} from "react";
+import React from "react";
 import {View, Image, StyleSheet, NativeSyntheticEvent, ImageErrorEventData} from "react-native";
 import {Color} from "../../../../styles/Color";
 import {getImageSize, getCachedImageSize, ImageSize} from "../../../../utils/media/image";
@@ -9,43 +9,65 @@ export interface CardMediaImageProps {
     content: CardContentModel;
     height?: number;
 }
+export interface CardMediaImageState {
+    imageSize?: ImageSize;
+    error?: string;
+}
 
-export function CardMediaImage(props: CardMediaImageProps) {
-    const uri = props.content.value;
-    const [imageSize, setImageSize] = useState<ImageSize|undefined>(getCachedImageSize(uri));
-    const [error, setError] = useState<string|undefined>();
-    const height = props.height || imageSize?.height || 0;
+export class CardMediaImage extends React.Component<CardMediaImageProps, CardMediaImageState> {
+    state = {} as CardMediaImageState;
 
-    const onLoadEnd = useCallback(async () => {
-        const size = await getImageSize(uri);
-        if (size.error) {
-            console.warn('CardMediaImage - Failed to measure image.', {uri, error: size.error});
-            setError('Failed to measure image.');
-        } else {
-            setImageSize(size);
-        }
-    }, [uri]);
-
-    const onError = useCallback((error: NativeSyntheticEvent<ImageErrorEventData>) => {
-        const message = 'Failed to load image.';
-        console.warn('CardMediaImage', message, {uri, error: error.nativeEvent.error});
-        setError(message);
-    }, [uri]);
-
-    if (error) {
-        return <CardMediaError message={error} height={height} />;
+    get uri() {
+        return this.props.content.value;
     }
 
-    return <View style={styles.root}>
-        <Image
-            source={{uri}}
-            resizeMode="contain"
-            onLoadEnd={!imageSize && !error ? onLoadEnd : undefined}
-            onError={onError}
-            style={{ width: '100%', height }}
-            fadeDuration={0}
-        />
-    </View>;
+    componentDidUpdate(prevProps: Readonly<CardMediaImageProps>/*, prevState: Readonly<CardMediaImageState>, snapshot?: any*/) {
+        if (prevProps.content.value !== this.props.content.value) { // URI changed
+            this.setState({
+                imageSize: getCachedImageSize(this.props.content.value),
+                error: undefined,
+            });
+        }
+    }
+
+    onLoadEnd = async () => {
+        await this.measureImage();
+    }
+
+    onError = (error: NativeSyntheticEvent<ImageErrorEventData>) => {
+        const message = `Failed to load image.`;
+        console.warn('CardMediaImage', message, { uri: this.uri, error: error.nativeEvent.error });
+        this.setState({ error: message });
+    }
+
+    async measureImage() {
+        const imageSize = await getImageSize(this.uri);
+        if (imageSize.error) {
+            console.warn('CardMediaImage - Failed to measure image.', { uri: this.uri, error: imageSize.error });
+            this.setState({ error: 'Failed to measure image.' });
+        } else {
+            this.setState({ imageSize })
+        }
+    }
+
+    render() {
+        const {error, imageSize} = this.state;
+        const height = this.props.height || imageSize?.height || undefined;
+
+        if (error) {
+            return <CardMediaError message={error} height={height} />;
+        }
+        return <View style={styles.root}>
+            <Image
+                source={{ uri: this.uri }}
+                resizeMode="contain"
+                onLoadEnd={!imageSize && !error ? this.onLoadEnd : undefined}
+                onError={this.onError}
+                style={{ width: '100%', height }}
+                fadeDuration={0}
+            />
+        </View>;
+    }
 }
 
 const styles = StyleSheet.create({
