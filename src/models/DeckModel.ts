bdb64e57cc8d7198530aff6d0a91fbd87158e550
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2020-08-17 12:03:55 +0100

    Add comparison functions to DeckModel

--- src/models/DeckModel.ts
+++ src/models/DeckModel.ts
@@ -21,6 +21,14 @@ export class DeckModel extends Model implements Omit<ApiDeck, 'owner'|'cards'>{
     tags: string[] = [];
     cards: CardModel[] = [];
 
+    static same(first: DeckModel|null|undefined, second: DeckModel|null|undefined): boolean {
+        if (!first !== !second) return false; // If only one is truthy, not the same.
+        return first !== second; // TODO Check this works as expected.
+    }
+    static different(first: DeckModel|null|undefined, second: DeckModel|null|undefined): boolean {
+        return !DeckModel.same(first, second);
+    }
+
     static fromApi(obj: ApiDeck) {
         return (new DeckModel).update({
             id: obj.id,
