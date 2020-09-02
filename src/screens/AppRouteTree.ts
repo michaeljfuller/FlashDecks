cbbdbd3a7fa65a017b5d24dd43436e1e2e2f7e22
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2020-09-02 16:17:36 +0100

    Add NavigationTree type

--- src/screens/AppRouteTree.ts
+++ src/screens/AppRouteTree.ts
@@ -1,5 +1,6 @@
 import {AppRoutes} from './AppRoutes';
 import DeckRouteTree from './Deck/DeckRouteTree';
+import {NavigationTree} from "./screens_types";
 
 export const AppRoutesTree = {
     base: AppRoutes.Home,
@@ -8,7 +9,7 @@ export const AppRoutesTree = {
         [AppRoutes.Temp]: null,
         [AppRoutes.Decks]: DeckRouteTree,
     }
-};
+} as NavigationTree;
 
 export {AppRoutes};
 export * from './Deck/DeckRouteTree';
