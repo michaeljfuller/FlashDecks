ce635b106780f835583ddee66a13fb48650098d2
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2020-09-25 16:43:13 +0100

    Add `GetCognitoUser` Lambda function
    `amplify add function`
    ? Select which capability you want to add: Lambda function (serverless function)
    ? Provide a friendly name for your resource to be used as a label for this category in the project: GetCognitoUser
    ? Provide the AWS Lambda function name: FlashDecks-GetCognitoUser
    ? Choose the runtime that you want to use: NodeJS
    ? Choose the function template that you want to use: Hello World
    ? Do you want to access other resources in this project from your Lambda function? Yes
    ? Select the category auth
    ? Select the operations you want to permit for FlashDecksAmplifyAuth read
    ? Do you want to invoke this function on a recurring schedule? No
    ? Do you want to configure Lambda layers for this function? No
    ? Do you want to edit the local lambda function now? No

---
+++ amplify/backend/function/GetCognitoUser/src/index.js
@@ -0,0 +1,18 @@
+/* Amplify Params - DO NOT EDIT
+	AUTH_FLASHDECKSAMPLIFYAUTH_USERPOOLID
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*"
+    //  }, 
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
