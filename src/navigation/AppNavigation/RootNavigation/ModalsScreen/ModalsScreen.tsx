0bd7eea585dd61641bb4fdf16e4a92400794e07b
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2020-11-06 15:27:26 +0000

    Add close button to "No modal found" ModalScreen state.

--- src/navigation/AppNavigation/RootNavigation/ModalsScreen/ModalsScreen.tsx
+++ src/navigation/AppNavigation/RootNavigation/ModalsScreen/ModalsScreen.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import {Text} from "react-native";
+import {Text, View} from "react-native";
 import {NavigationScreenProps} from "../../../navigation_types";
+import Button from "../../../../components/button/Button";
 import {PortalExit} from "../../../../components/portal/PortalExit";
 import {modalPortalId} from "../../../../components/modal/core/Modal.common";
-import {ModalsScreenProps} from "../actions";
+import {ModalsScreenProps, navigateFromModal} from "../actions";
 
 export function ModalsScreen(_: ModalsScreenProps & NavigationScreenProps) {
     return <PortalExit portalId={modalPortalId}>
-        <Text>No modal found.</Text>
+        <View style={{ height: '100%', padding: 5 }}>
+            <Text>No modal found.</Text>
+        </View>
+        <Button title="Close" onClick={navigateFromModal} square style={{
+            position: "absolute",
+            bottom: 0,
+        }} />
     </PortalExit>;
 }
 export default ModalsScreen;
