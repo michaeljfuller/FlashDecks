50d6bf97ee76b0696820661acc34cb0ef5a88361
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2020-11-16 14:53:40 +0000

    Make renameClass a decorator too.

--- src/utils/class.ts
+++ src/utils/class.ts
@@ -3,7 +3,31 @@ export interface GenericClass<T extends {} = {}> extends Function {
     prototype: T;
 }
 
-export function renameClass<T = any>(name: string, constructor: T): T {
-    Object.defineProperty(constructor, 'name', {value: name, writable: false});
-    return constructor;
+//<editor-fold desc="renameClass">
+
+/** Function to rename the passed class */
+export function renameClass<T extends Function>(name: string, constructor: T): T;
+
+/** Decorator to rename the passed class */
+export function renameClass(name: string): ClassDecorator;
+
+/**
+ * Renames a class. Can be used as a function or decorator.
+ * @example
+ *  renameClass('FooBar', FooClass);
+ * @example
+ *  @renameClass('FooBar')
+ *  class FooClass {
+ */
+export function renameClass<T extends Function>(name: string, constructor?: T): ClassDecorator|T {
+    if (constructor) {
+        Object.defineProperty(constructor, 'name', {value: name, writable: false});
+        return constructor;
+    } else {
+        return function (constructor: Function) {
+            renameClass(name, constructor);
+        };
+    }
 }
+
+//</editor-fold>
