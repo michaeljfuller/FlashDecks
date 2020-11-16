247635e667a6b0100a1625bc9786396a62ad4ae2
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2020-11-16 18:36:22 +0000

    Back button on ModalsScreen closes top modal
    Previous behavior was to navigate back from ModalsScreen, which would close all modals (if multiple were open).

--- src/navigation/AppNavigation/RootNavigation/ModalsScreen/ModalsScreen.tsx
+++ src/navigation/AppNavigation/RootNavigation/ModalsScreen/ModalsScreen.tsx
@@ -1,20 +1,38 @@
 import React from "react";
-import {Text, View} from "react-native";
+import {Text, View, BackHandler, NativeEventSubscription} from "react-native";
 import {NavigationScreenProps} from "../../../navigation_types";
+import portalStore from "../../../../store/portals/PortalStore";
 import Button from "../../../../components/button/Button";
 import {PortalExit} from "../../../../components/portal/PortalExit";
 import {modalPortalId} from "../../../../components/modal/core/Modal.common";
 import {ModalsScreenProps, navigateFromModal} from "../actions";
 
-export function ModalsScreen(_: ModalsScreenProps & NavigationScreenProps) {
-    return <PortalExit portalId={modalPortalId} onEmpty={navigateFromModal}>
-        <View style={{ height: '100%', padding: 5 }}>
-            <Text>No modal found.</Text>
-        </View>
-        <Button title="Close" onClick={navigateFromModal} square style={{
-            position: "absolute",
-            bottom: 0,
-        }} />
-    </PortalExit>;
+export class ModalsScreen extends React.PureComponent<ModalsScreenProps & NavigationScreenProps> {
+
+    hardwareBackPress?: NativeEventSubscription;
+
+    componentDidMount() {
+        this.hardwareBackPress = BackHandler.addEventListener('hardwareBackPress', this.onBackPress.bind(this));
+    }
+    componentWillUnmount() {
+        this.hardwareBackPress?.remove();
+    }
+
+    onBackPress() {
+        portalStore.getEntrances(modalPortalId)[0]?.close(); // Close the current entrance.
+        return true; // Prevent default
+    }
+
+    render() {
+        return <PortalExit portalId={modalPortalId} onEmpty={navigateFromModal}>
+            <View style={{ height: '100%', padding: 5 }}>
+                <Text>No modal found.</Text>
+            </View>
+            <Button title="Close" onClick={navigateFromModal} square style={{
+                position: "absolute",
+                bottom: 0,
+            }} />
+        </PortalExit>;
+    }
 }
 export default ModalsScreen;
