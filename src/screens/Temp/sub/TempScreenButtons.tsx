3c098b98a04f7fb3370fab11757bd6cddc5d58f1
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2021-01-11 22:30:07 +0000

    TempScreenButtons showcases buttons better,
    More cases and clearer labeling.

--- src/screens/Temp/sub/TempScreenButtons.tsx
+++ src/screens/Temp/sub/TempScreenButtons.tsx
@@ -1,40 +1,89 @@
 import React from "react";
-import {Button} from "../../../components/button/Button";
+import {StyleSheet, Text, View} from "react-native";
+import {Button, IconType} from "../../../components/button/Button";
 import TextButton from "../../../components/button/TextButton";
-import IconButton, {IconType} from "../../../components/button/IconButton";
-import Row from "../ui/TempScreenRow";
+import IconButton from "../../../components/button/IconButton";
+import Row from "../../../components/layout/Row";
 import TempScreenSubsection from "../ui/TempScreenSubsection";
 
 export function TempScreenButtons() {
     const noop = React.useCallback(() => {}, []);
 
     return <TempScreenSubsection title="Buttons">
-        <Row>
-            <Button title="Button" onClick={noop} square />
-            <TextButton title="TextButton" onClick={noop} color="Blue" />
-            <IconButton icon={IconType.QuestionMark} onClick={noop} color="Blue" text="Blue" />
-            <IconButton icon={IconType.QuestionMark} onClick={noop} color="Blue" />
-            <IconButton icon={IconType.QuestionMark} onClick={noop} color="Blue" flat />
-            <IconButton icon={IconType.QuestionMark} onClick={noop} color="Blue" transparent />
+        <Text>Button</Text>
+        <Row style={styles.row} scroll>
+            <Button onClick={noop} title="Standard" />
+            <Button onClick={noop} title="Square"       color="Green" square />
+            <Button onClick={noop} icon={IconType.Info} color="Orange" iconPosition="left"  title="Icon" />
+            <Button onClick={noop} icon={IconType.Info} color="Red"    iconPosition="right" title="Icon" />
+            <Button onClick={noop} icon={IconType.Info} color="Black"  iconPosition="right" />
+            <Button onClick={noop} title="Inverted" invertColor />
+            <Button onClick={noop} title="Flat" flat />
+            <Button onClick={noop} title="Disabled" disabled />
         </Row>
-        <Row borderColor='red' backgroundColor='#fee'>
-            <Button title="Button" onClick={noop} color="Red" flat={true} square />
-            <TextButton title="TextButton" onClick={noop} color="Red" />
-            <IconButton icon={IconType.Home} onClick={noop} text="Red" color="Red" />
-            <IconButton icon={IconType.Home} onClick={noop} text="Home" color="Red" transparent />
+
+        <Text>IconButton</Text>
+        <Row style={styles.row} scroll>
+            <Row style={styles.iconButtonWrapper}>
+                <IconButton onClick={noop} icon={IconType.Info} />
+                <IconButton onClick={noop} icon={IconType.Info} color="Green" />
+                <IconButton onClick={noop} icon={IconType.Info} color="Orange" />
+                <IconButton onClick={noop} icon={IconType.Info} color="Red" />
+                <IconButton onClick={noop} icon={IconType.Info} color="Black" />
+            </Row>
+            <Row style={styles.iconButtonWrapper}>
+                <IconButton onClick={noop} icon={IconType.Info} invertColor />
+                <Text style={styles.iconLabel}>Inverted</Text>
+            </Row>
+            <Row style={styles.iconButtonWrapper}>
+                <IconButton onClick={noop} icon={IconType.Info} flat />
+                <Text style={styles.iconLabel}>Flat</Text>
+            </Row>
+            <Row style={styles.iconButtonWrapper}>
+                <IconButton onClick={noop} icon={IconType.Info} transparent />
+                <Text style={styles.iconLabel}>Transparent</Text>
+            </Row>
+            <Row style={styles.iconButtonWrapper}>
+                <IconButton onClick={noop} icon={IconType.Info} disabled />
+                <Text style={styles.iconLabel}>Disabled</Text>
+            </Row>
         </Row>
-        <Row borderColor='green' backgroundColor='#efe'>
-            <Button title="Button" onClick={noop} color="Green" square />
-            <TextButton title="TextButton" onClick={noop} color="Green" />
-            <IconButton icon={IconType.Menu} onClick={noop} text="Green" color="Green" />
-            <IconButton icon={IconType.Menu} onClick={noop} text="Menu" color="Green" transparent />
+
+        <Text>IconButton with text</Text>
+        <Row style={styles.row} scroll>
+            <IconButton onClick={noop} icon={IconType.Info} text="Text" />
+            <IconButton onClick={noop} icon={IconType.Info} text="Inverted" invertColor color="Green" />
+            <IconButton onClick={noop} icon={IconType.Info} text="Flat" flat color="Orange" />
+            <IconButton onClick={noop} icon={IconType.Info} text="Transparent" transparent color="Red" />
+            <IconButton onClick={noop} icon={IconType.Info} text="Disabled" disabled />
         </Row>
-        <Row borderColor='grey' backgroundColor='#eee'>
-            <Button title="Disabled Button" square />
-            <TextButton title="Disabled TextButton" onClick={noop} disabled={true} color="Grey" />
-            <IconButton icon={IconType.QuestionMark} color="White" />
-            <IconButton icon={IconType.QuestionMark} color="Black" transparent />
+
+        <Text>TextButton</Text>
+        <Row style={styles.row} scroll>
+            <TextButton onClick={noop} title="Standard" />
+            <TextButton onClick={noop} title="Red" color="Red" />
+            <TextButton onClick={noop} title="Green" color="Green" />
+            <TextButton onClick={noop} title="Inverted White" invertColor color="White" />
+            <TextButton onClick={noop} title="Disabled" disabled />
         </Row>
+
     </TempScreenSubsection>;
 }
 export default TempScreenButtons;
+
+const styles = StyleSheet.create({
+    row: {
+        borderWidth: 1,
+        borderColor: "grey",
+        padding: 3,
+        marginHorizontal: 2,
+        marginBottom: 10,
+    },
+    iconLabel: {
+        paddingHorizontal: 2,
+        lineHeight: 24,
+    },
+    iconButtonWrapper: {
+        marginRight: 5,
+    }
+});
