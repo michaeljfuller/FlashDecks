92e84a8ef86c5ab944135733b99f0abec60894ef
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2021-01-12 20:52:30 +0000

    Use full width buttons as subsection titles.

--- src/screens/Temp/sub/TempScreenButtons.tsx
+++ src/screens/Temp/sub/TempScreenButtons.tsx
@@ -13,9 +13,7 @@ export function TempScreenButtons() {
 
     return <TempScreenSubsection title="Buttons">
 
-        <Button title="Button" onClick={noop} style={{margin:2}} />
-
-        <Text>Standard Button</Text>
+        <TitleButton title="Button" />
         <ScrollRow>
             <Button onClick={noop} title="Standard" />
             <Button onClick={noop} title="Square"      square      color="Green" />
@@ -27,17 +25,7 @@ export function TempScreenButtons() {
             <Button onClick={noop} title="Disabled"    disabled />
         </ScrollRow>
 
-        <Text>Transparent button</Text>
-        <ScrollRow>
-            <Button onClick={noop} transparent title="Standard" />
-            <Button onClick={noop} transparent title="Square"      square      color="Green"  />
-            <Button onClick={noop} transparent title="Square Flat" square flat color="Orange" />
-            <Button onClick={noop} transparent title="Flat"        flat        color="Red"    />
-            <Button onClick={noop} transparent title="Inverted"    invertColor color="White"  />
-            <Button onClick={noop} transparent title="Disabled"    disabled />
-        </ScrollRow>
-
-        <Text>Button with icon</Text>
+        <TitleButton title="Button (with icon)" icon={IconType.Info} />
         <ScrollRow>
             <Button onClick={noop} icon={IconType.Info} title="Standard" />
             <Button onClick={noop} icon={IconType.Info} title="Left"     color="Green"  iconPosition="left" />
@@ -50,7 +38,17 @@ export function TempScreenButtons() {
             <Button onClick={noop} icon={IconType.Info} title="Disabled" disabled />
         </ScrollRow>
 
-        <Text>Transparent button with icon</Text>
+        <TitleButton title="Button (transparent)" transparent />
+        <ScrollRow>
+            <Button onClick={noop} transparent title="Standard" />
+            <Button onClick={noop} transparent title="Square"      square      color="Green"  />
+            <Button onClick={noop} transparent title="Square Flat" square flat color="Orange" />
+            <Button onClick={noop} transparent title="Flat"        flat        color="Red"    />
+            <Button onClick={noop} transparent title="Inverted"    invertColor color="White"  />
+            <Button onClick={noop} transparent title="Disabled"    disabled />
+        </ScrollRow>
+
+        <TitleButton title="Button (transparent with icon)" transparent icon={IconType.Info} />
         <ScrollRow>
             <Button onClick={noop} transparent icon={IconType.Info} title="Standard" />
             <Button onClick={noop} transparent icon={IconType.Info} title="Left"   color="Green"  iconPosition="left" />
@@ -63,15 +61,16 @@ export function TempScreenButtons() {
             <Button onClick={noop} transparent icon={IconType.Info} title="Disabled" disabled />
         </ScrollRow>
 
-        <Text>LinkButton</Text>
+        <LinkButton url="http://www.google.com" title="LinkButton" icon={IconType.Exit} iconPosition="right" square flat style={styles.titleButton} />
         <ScrollRow>
             <LinkButton url="http://www.google.com" />
+            <LinkButton />
             <LinkButton url="http://www.google.com" title="Icon"        color="Green"  icon={IconType.Info} />
             <LinkButton url="http://www.google.com" title="Square"      color="Orange" square/>
             <LinkButton url="http://www.google.com" title="Flat"        color="Red"    flat />
             <LinkButton url="http://www.google.com" title="Inverted"    color="White"  invertColor />
-            <LinkButton url="http://www.google.com" title="Disabled"    disabled />
-            <LinkButton url="http://www.google.com" title="Transparent" color="Black"  transparent />
+            <LinkButton url="http://www.google.com" title="Disabled"    color="Blue"   disabled />
+            <LinkButton url="http://www.google.com" title="Transparent" color="Black" transparent />
         </ScrollRow>
 
         <Text style={{
@@ -130,6 +129,9 @@ export function TempScreenButtons() {
 export default TempScreenButtons;
 
 const styles = StyleSheet.create({
+    titleButton: {
+        marginHorizontal: 2,
+    },
     row: {
         borderWidth: 1,
         borderColor: "grey",
@@ -145,6 +147,12 @@ const styles = StyleSheet.create({
     }
 });
 
+const TitleButton = withDefaultProps(Button, {
+    flat: true,
+    square: true,
+    onClick: () => {},
+    style: styles.titleButton,
+});
 const ScrollRow = withDefaultProps(Row, {
     scroll: true,
     style: styles.row,
