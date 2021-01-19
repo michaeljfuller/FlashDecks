97d8a15c2b042ae312b0470d8db998d7c1ef90a6
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2021-01-19 14:38:56 +0000

    Allow other View props in Visibility component

--- src/components/layout/Visibility.tsx
+++ src/components/layout/Visibility.tsx
@@ -1,21 +1,21 @@
 import React from "react";
-import {StyleProp, StyleSheet, View, ViewStyle} from "react-native";
+import {StyleProp, StyleSheet, View, ViewProps, ViewStyle} from "react-native";
 
-export type VisibilityProps = React.PropsWithChildren<{
+export interface VisibilityProps extends ViewProps {
     render?: boolean; // Keep size while not displaying
     visible?: boolean; // Set false to prevent children from being constructed
-    style?: StyleProp<ViewStyle>;
-}>;
+}
 
 /** A view that can either hide its children, or stop them being created. */
-export function Visibility(props: VisibilityProps) {
-    const { render=true, visible=true } = props;
+export function Visibility(props: React.PropsWithChildren<VisibilityProps>) {
+    const { render=true, visible=true, style, ...viewProps } = props;
     if (!render) return null;
-    
-    const style: StyleProp<ViewStyle> = [];
-    if (props.style) Array.isArray(props.style) ? style.push(...props.style) : style.push(props.style);
 
-    return <View style={[...style, visible ? null : styles.invisible]}>
+    const viewStyles: StyleProp<ViewStyle> = [];
+    if (props.style) Array.isArray(style) ? viewStyles.push(...style) : viewStyles.push(style);
+    if (!visible) viewStyles.push(styles.invisible);
+
+    return <View {...viewProps} style={viewStyles}>
         {props.children}
     </View>;
 }
