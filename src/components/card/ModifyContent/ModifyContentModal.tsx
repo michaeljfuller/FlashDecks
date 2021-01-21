cdbec70ada7f01cf0f4c5a5f7194eaaf3fd141ae
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2021-01-21 16:42:20 +0000

    Fix media type buttons to top of ModifyContentModal

--- src/components/card/ModifyContent/ModifyContentModal.tsx
+++ src/components/card/ModifyContent/ModifyContentModal.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Text, StyleSheet, View} from "react-native";
+import {Text, StyleSheet} from "react-native";
 
 import Button from "../../button/Button";
 import Modal, {ModalProps, extractModalProps} from "../../modal/core/Modal";
@@ -8,6 +8,7 @@ import {ModalFooter} from "../../modal/parts";
 import {CardContentModel, CardContentType, cardContentTypes} from "../../../models";
 import {CardContentForm} from "./CardContentForm";
 import Row from "../../layout/Row";
+import Column from "../../layout/Column";
 
 export type ModifyContentModalProps = {
     title?: string;
@@ -47,7 +48,7 @@ export class ModifyContentModal extends React.PureComponent<ModifyContentModalPr
         return <Modal {...extractModalProps(this.props)}>
             <ModalHeader title={this.props.title || "Modify Content"} />
 
-            <ModalBody style={styles.body}>
+            <ModalBody style={styles.body} center={false}>
                 <Row overflow center style={styles.typeButtonRow}>{ // Type Buttons
                     cardContentTypes.map(
                         contentType => <Button square
@@ -59,11 +60,13 @@ export class ModifyContentModal extends React.PureComponent<ModifyContentModalPr
                         />
                     )
                 }</Row>
+                <Column center flex>
                 {   // Form
                     this.props.content.validType
                     ? <CardContentForm content={this.props.content} onChange={this.onChange} preview />
                     : <Text style={styles.centerText}>Please select a content type.</Text>
                 }
+                </Column>
             </ModalBody>
 
             <ModalFooter style={styles.footer}>
