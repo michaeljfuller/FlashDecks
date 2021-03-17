97703b119a5e5f3ed9c49f132719f05633085220
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2021-03-17 17:54:07 +0000

    Update createRenderComponent.withRedux to allow FunctionComponent

--- test/react-testing-library/createRenderComponent.tsx
+++ test/react-testing-library/createRenderComponent.tsx
@@ -3,7 +3,6 @@ import {combineReducers, createStore, PreloadedState} from "redux";
 import {Provider} from "react-redux";
 import {render} from "@testing-library/react";
 import {addPropertiesToFunction} from "../../src/utils/function";
-import {ComponentProps, ComponentUnion} from "../../src/utils/component";
 import {reducerMap, StoreState} from "../../src/store/store_manifest";
 import {AppStore} from "../../src/store/store";
 
@@ -17,29 +16,27 @@ export const createRenderComponent = addPropertiesToFunction(
 type RenderOptions = Parameters<typeof render>[1];
 
 /** Create a function that renders a component using testing-library, while defining default properties. */
-function _createRenderComponent<
-    P extends {}
->(
-    component: FunctionComponent<P> | ComponentClass<P>,
-    defaultProps: P,
+function _createRenderComponent<Props extends {}>(
+    component: FunctionComponent<Props> | ComponentClass<Props>,
+    defaultProps: Props,
     defaultOptions?: RenderOptions,
 ) {
     return (
-        props?: Partial<P>,
+        props?: Partial<Props>,
         options?: RenderOptions
     ) => {
         options = Object.assign({}, defaultOptions, options);
-        const element = React.createElement(component, Object.assign({}, defaultProps, props));
+        const element = React.createElement(
+            component,
+            Object.assign({}, defaultProps, props)
+        );
         return render(element, options);
     }
 }
 
 /** Create a function that renders a component using testing-library, while defining default properties & store. */
-function _createRenderReduxComponent<
-    Component extends ComponentUnion,
-    Props = ComponentProps<Component>,
->(
-    component: Component,
+function _createRenderReduxComponent<Props extends {}>(
+    component: FunctionComponent<Props> | ComponentClass<Props>,
     defaultProps?: Props,
     defaultStoreState?: Partial<PreloadedState<StoreState>>,
     defaultOptions?: RenderOptions,
@@ -51,13 +48,15 @@ function _createRenderReduxComponent<
         options?: RenderOptions,
     ) => {
         const store: AppStore = createStore(rootReducer, storeState || defaultStoreState);
-        props = Object.assign({}, defaultProps, props);
         options = Object.assign({}, defaultOptions, {
             wrapper: function StoreWrapper(props: PropsWithChildren<any>) {
                 return <Provider store={store}>{props.children}</Provider>;
             },
         }, options);
-        const element = React.createElement(component, props);
+        const element = React.createElement(
+            component,
+            Object.assign({}, defaultProps, props)
+        );
         return {store, ...render(element, options)};
     };
 }
